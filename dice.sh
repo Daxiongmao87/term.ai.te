650304aa77c59e27dcc364594b397c672d288d05
daxiongmao87@gmail.com <daxiongmao87@gmail.com>
2025-05-09 14:14:13 -0500

    bookmark

--- dice.sh
+++
@@ -1,8 +0,0 @@
-if [[ $input =~ ^([0-9]+)d([0-9]+)(\+([0-9]+))? ]]; then
-    num=${BASH_REMATCH[1]}
-    sides=${BASH_REMATCH[2]}
-    modifier=${BASH_REMATCH[4]:-0}
-else
-    echo "Invalid syntax: \$input" >&2
-    exit 1
-fi
